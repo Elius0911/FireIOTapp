7624ada40179a56ad712328fe0365a5e4e3b28a4
Zypheus <c25887@gmail.com>
2022-09-13 17:20:50 +0800

    gg...

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,29 @@
+import 'dart:async';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'noti.dart';
+import 'NotificationService.dart';
 import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
 }
 
-void alarmSensor() async {
-  var url = Uri.http('192.168.21.34', 'index.php', {'alarm': '1'});
-  var response = await http.get(url);
-  if (response.statusCode == 200) {
-    print('cc');
-  }
+Future alarmSensor() async {
+  var url = Uri.http('192.168.21.34', '/espImport.php', {'DataPack': '1'});
+  bool alarm = false;
+
+  Timer.periodic(Duration(seconds: 1), (timer) async {
+    var response = await http.get(url);
+    print('Response body: ${response.body}');
+
+    if (alarm == true) {
+      NotificationService().showNotification(1, "cc", "嘻嘻");
+      alarm = false;
+    }
+  });
 }
 
 class MyApp extends StatelessWidget {
@@ -23,7 +34,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'WebView',
-      home: MainScreen(),
+      home: WebViewApp(),
     );
   }
 }
@@ -33,6 +44,7 @@ class WebViewApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    alarmSensor();
     return Scaffold(
       body: WebView(
         initialUrl: 'http://192.168.21.34/index.php',
