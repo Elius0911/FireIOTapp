e9baf9c008f18197220cef8c86f6f979f5240ad1
Zypheus <c25887@gmail.com>
2022-09-20 17:01:48 +0800

    json Decode & notification

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,41 @@
-import 'dart:async';
-
-import 'package:flutter/foundation.dart';
+import 'dart:async'; //異步處理
+import 'dart:convert'; //json 轉換
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
-import 'noti.dart';
-import 'NotificationService.dart';
 import 'package:http/http.dart' as http;
+
+import 'NotificationService.dart';
+
+String url = 'http://192.168.21.34/'; //TODO: IP
+int periodTime = 1; //TODO: 週期時間
 
 void main() {
   runApp(const MyApp());
 }
 
-Future alarmSensor() async {
-  var url = Uri.http('192.168.21.34', '/espImport.php', {'DataPack': '1'});
-  bool alarm = false;
+//json 解析 & 警報
+Future jsonDecode_and_alarm() async {
+  var decode; //json 解析後的資料
+  int value; //是否起火或有煙霧
+  String warningText; // 警告字樣
 
-  Timer.periodic(Duration(seconds: 1), (timer) async {
-    var response = await http.get(url);
-    print('Response body: ${response.body}');
+  //週期執行
+  Timer.periodic(Duration(seconds: periodTime), (timer) async {
+    final response = await http.get(Uri.parse(url + 'alarm.json')); //http 請求
 
-    if (alarm == true) {
-      NotificationService().showNotification(1, "cc", "嘻嘻");
-      alarm = false;
+    //如果 Server 回應 200(接受)，則解析 json 檔
+    if (response.statusCode == 200) {
+      decode = jsonDecode(response.body);
+      value = decode['alarm'];
+    } else {
+      throw Exception('no data ;(');
+    }
+
+    //若起火或有煙霧，則發出警報
+    if (value == 1) {
+      warningText = decode['warningText'];
+      //通知(ID, 標題, 內容)
+      NotificationService().showNotification(1, "居家防災警報", warningText);
     }
   });
 }
@@ -39,15 +53,16 @@ class MyApp extends StatelessWidget {
   }
 }
 
+//連結主網頁
 class WebViewApp extends StatelessWidget {
   const WebViewApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    alarmSensor();
+    jsonDecode_and_alarm(); //TODO: 拉走
     return Scaffold(
       body: WebView(
-        initialUrl: 'http://192.168.21.34/index.php',
+        initialUrl: url + 'index.php',
         javascriptMode: JavascriptMode.unrestricted,
       ),
     );
